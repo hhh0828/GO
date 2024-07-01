504f7c5fc69c94521106c39b1c00df869ca78d1d
hyunho.hong <hhhcjswo@naver.com>
2024-07-01 14:17:23 +0900

    2:17 0701

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -8,12 +8,18 @@ import (
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
+
+	name1 := []string{
+		"hyunho",
+		"soyeon",
+	}
 	// If no name was given, return an error with a message.
 	if name == "" {
 		return name, errors.New("empty name")
 	}
+
 	// Create a message using a random format.
-	message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFormat(), name+","+name1[rand.Intn(2)])
 	return message, nil
 }
 
