eac89dea5673fdeae6365c76b4f4ee660592a4d4
hyunho.hong <hhhcjswo@naver.com>
2024-07-01 18:15:59 +0900

    "variable input"

---
+++ variable_prac/var.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var a int
+	var b int
+	var c int
+	//var msg string = "hello variable"
+	stdin := bufio.NewReader(os.Stdin)
+
+	//a = 20
+
+	n, err := fmt.Scanln(&a, &b, &c)
+
+	if err != nil {
+		fmt.Println(err)
+		stdin.ReadString('\n')
+	} else {
+		fmt.Println(n, "개 입력받았다")
+		fmt.Println(a, b, c)
+	}
+
+}
