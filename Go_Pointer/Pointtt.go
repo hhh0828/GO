3cee39b8187d8ef8322835e2413971d3d7979a94
hyunho.hong <hhhcjswo@naver.com>
2024-09-25 13:13:45 +0900

    update pointer 개념

--- Go_Pointer/Pointtt.go
+++ Go_Pointer/Pointtt.go
@@ -2,19 +2,6 @@ package main
 
 import "fmt"
 
-type test struct {
-	a int
-	b [10]int
-}
-
-//구조체에 포인터를 입력을 해준다.... test의 포인터로 가라
-func changedata(arg *test) {
-	arg.a = 10
-	arg.b[9] = 1
-	fmt.Println(arg.a, arg.b)
-
-}
-
 //Go, can control pointer like c/c++ but can't use operator for operating.
 func main() {
 	//var a int
@@ -29,13 +16,48 @@ func main() {
 	//fmt.Println(p)
 	//fmt.Printf("%d", a)
 
-	var test1 test = test{
-		a: 0,
+	var data = Data{
+		Value: "t",
 	}
-	//주소값을 넣으면
-	changedata(&test1)
+	data.Array[100] = "t"
 
-	fmt.Println(test1.a, test1.b[9])
+	a := ChangeDatafunc2(data)
+	fmt.Println(a.Array[100])
+	fmt.Println(a.Value)
+
 }
 
 //Pointer의 대한 이해는 좌항은 공간 , 우항은 값 ...
+
+//Why Pointer ?
+
+/*
+함수 호출 시. 파라미터에 대한 값 복사(사이즈)가 발생함.
+복사된 구조체 또는 변수안에서 해당 함수의 내용이 실행되면서 실제 값이 안바뀌는 일이 발생할 수 있음.
+또한 불필요한 메모리사용이 발생 할 수있음, 배열의 사이즈가 큰경우
+
+포인터로 지정하면 8바이트의 주소값으로 같은 값을 가르킬수있다.
+예시 below
+*/
+
+type Data struct {
+	Value string
+	Array [200]string
+}
+
+// 바로 가르킴 8바이트... 메모리주소값만.. 복사됨
+func ChangeDatafunc(arg *Data) {
+	arg.Value = "test"
+	arg.Array[100] = "test1"
+}
+
+// 메모리 낭비
+func ChangeDatafunc2(arg Data) Data {
+	arg.Value = "test"
+	arg.Array[100] = "test1"
+	return arg
+}
+
+// 궁금점 함수 콜스택과 관련 또한 메모리 사용과 관련하여 가비지컬렉터가 언제 일하냐.
+// sweap and mark 마크 하고 지우고 마크하고 지우고...
+// 참조가 더이상 발생하지 않을때 제거
